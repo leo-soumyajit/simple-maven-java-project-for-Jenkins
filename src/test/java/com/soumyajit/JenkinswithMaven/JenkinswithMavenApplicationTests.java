8f682512c0a40562cc347ead39323a681495ce50
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-10-21 13:17:03 +0530

    added test func

--- src/test/java/com/soumyajit/JenkinswithMaven/JenkinswithMavenApplicationTests.java
+++ src/test/java/com/soumyajit/JenkinswithMaven/JenkinswithMavenApplicationTests.java
@@ -1,13 +1,34 @@
 package com.soumyajit.JenkinswithMaven;
 
+import com.soumyajit.JenkinswithMaven.Controller.ApiController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JenkinswithMavenApplicationTests {
 
+	private final ApiController controller = new ApiController();
+
 	@Test
-	void contextLoads() {
+	void testHomeEndpoint() {
+		String response = controller.home();
+		assertEquals("Greetings from Soumyajit Banerjee!", response);
+	}
+
+	@Test
+	void testHelloEndpointWithName() {
+		String response = controller.hello(Optional.of("Soumyajit"));
+		assertEquals("Hello, Soumyajit! Welcome to the Maven Project.", response);
+	}
+
+	@Test
+	void testHelloEndpointWithDefaultName() {
+		String response = controller.hello(Optional.empty());
+		assertEquals("Hello, Super Coder! Welcome to the Maven Project.", response);
 	}
 
 }
